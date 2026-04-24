6f0dc48c2616bb969d278b7818139b15931ed52f
Peafowll <tudorpaun16@gmail.com>
2026-04-24 14:46:39 +0300

    feat : weather api

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import json
+import requests
 from datetime import datetime
 
 def record_historical_data(data : dict, timestamp : datetime):
@@ -14,13 +15,38 @@ def record_historical_data(data : dict, timestamp : datetime):
     with open("historical_data.json", "w") as historical_file:
         json.dump(loaded_data, historical_file, indent=4)
 
-if __name__ == "__main__":
-    current_data = {
-        "temp" : 40,
-        "air" : 60,
-        "humidity" : 70,
-        "light" : 0,
-        "window_open" : True
+
+def get_current_weather(location : tuple):
+    """
+    Gets current weather for location.
+    Params :
+        location (tuple) : A tuple containing the latitude and logitude of the location you want to check.
+    Returns :
+        The temperature in Celsius and wind speed at the location.
+    
+    """
+    url = "https://api.open-meteo.com/v1/forecast"
+    
+    params = {
+        "latitude": location[0],
+        "longitude": location[1],
+        "current_weather": True
     }
 
-    record_historical_data(data=current_data, timestamp=datetime.now())
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        weather_data = response.json()
+        
+        current = weather_data['current_weather']
+        
+
+        return current["temperature"], current["windspeed"]
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+
+
+if  __name__ ==  "__main__":
+    location_tuple = (45.6486, 225.6061)
+    get_current_weather(location_tuple)
